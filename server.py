9a49458802f68e74692a96ea2fe91dd60f7dc04e
amos <amosjaul@gmail.com>
2016-10-08 22:52:56 -0400

    adding db
    Signed-off-by: amos <amosjaul@gmail.com>

---
+++ server.py
@@ -0,0 +1,12 @@
+from pymongo import MongoClient
+
+client = MongoClient()
+
+def fetch_amount_of_food():
+    return 10
+
+def fetch_current_list():
+    return []
+
+def generate_healthier_list( user ):
+     
